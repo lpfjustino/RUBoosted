7bec9d7b61b1bc54433f920ed1ea233a6bf01f2c
lpfjustino <lpfjustino@gmail.com>
2017-06-09 14:00:24 -0300

    Visualization is now working properly

--- visualization.py
+++ visualization.py
@@ -1,6 +1,8 @@
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import pylab
 
 def show_3d(data):
     fig = plt.figure()
@@ -20,9 +22,18 @@ def show_3d(data):
     plt.show()
 
 def show_2d(data):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xlabel("KDA", fontsize=12)
+    ax.set_ylabel("Dmg", fontsize=12)
+    ax.grid(True, linestyle='-', color='0.75')
+    # colors = ['blue', 'cyan', 'green', 'orange', 'red']
+    colors = ['brown', 'gray', 'orange', 'blue', 'red']
+
     xs = data[:,0]
     ys = data[:,1]
-    colors = data[:,2]
-    plt.scatter(xs, ys, c=colors)
+    labels = data[:,2]
+
+    ax.scatter(xs, ys, c=labels, cmap=matplotlib.colors.ListedColormap(colors))
 
     plt.show()
